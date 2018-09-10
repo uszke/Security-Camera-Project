d97a2ddd54a3782d4dd17afeb56325b1b30f427e
lassecod2 <39300644+lassecod2@users.noreply.github.com>
2018-09-10 14:33:54 +0200

    Add files via upload

--- openCV1.py
+++ openCV1.py
@@ -1,16 +1,41 @@
 import numpy as np
 import cv2
+import serial
+import time
 
-# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
+ser = serial.Serial()
 
-# https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+for pos in range (80,300,5):
+    standby = pos
+
+def setup():
+    ser.baudrate = 115200
+    ser.port = 'COM3'
+    ser.open()
+
+
+def setpantilt(x,y):
+    ser.write(bytearray([48, x]))
+    ser.write(bytearray([49, y]))
+
+
+setup()
+setpantilt(90, 40)
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-full_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+#eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+#full_cascade = cv2.CascadeClassifier('haarcascade_fullbody.xml')
 cap = cv2.VideoCapture(1)
 
+
+tiltX = 90
+tiltY = 40
+
+
+time.sleep(1)
 while 1:
+
+
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -21,6 +46,28 @@ while 1:
         roi_gray = gray[y:y + h, x:x + w]
         roi_color = img[y:y + h, x:x + w]
         print('X :', x, 'Y :', y)
+
+        if (x > 120 & x < 200):
+            print('Standby')
+        if (y > 70 & y < 140):
+            print('Standby')
+        if (x > 250):
+            tiltX -= 2
+            setpantilt(tiltX, tiltY)
+        if (x < 100):
+            tiltX += 2
+            setpantilt(tiltX, tiltY)
+        if(y > 30):
+            tiltY += 2
+            setpantilt(tiltX, tiltY)
+        if(y < 180):
+            tiltY -= 2
+            setpantilt(tiltX, tiltY)
+        else:
+            break
+
+
+
 
 
 
